2b7cd60f99752ea4a5c18a5ba2e55a495b337ee0
shladdergoo <shladdergoo@users.noreply.github.com>
2021-08-22 22:28:54 +0100

    NoteProcessorService depends on EventBus

--- note-processor/src/container/types.ts
+++ note-processor/src/container/types.ts
@@ -1,6 +1,7 @@
 const TYPES = {
   ProcessorService: Symbol.for('ProcessorService'),
   NoteRepository: Symbol.for('NoteRepository'),
+  EventBus: Symbol.for('EventBus'),
 };
 
 export { TYPES };
--- note-processor/src/note_processor_service.ts
+++ note-processor/src/note_processor_service.ts
@@ -1,6 +1,7 @@
 import { inject, injectable } from 'inversify';
 import { TYPES } from './container/types';
 
+import { EventBus } from './event_bus';
 import { Note } from './note';
 import { NoteRepository } from './note_repository';
 import { ProcessorService } from './processor_service';
@@ -8,14 +9,22 @@ import { ProcessorService } from './processor_service';
 @injectable()
 export class NoteProcessorService implements ProcessorService {
   private readonly _noteRepository: NoteRepository;
+  private readonly _eventBus: EventBus;
 
   public constructor(
-    @inject(TYPES.NoteRepository) noteRepository: NoteRepository
+    @inject(TYPES.NoteRepository) noteRepository: NoteRepository,
+    @inject(TYPES.EventBus) eventBus: EventBus
   ) {
     this._noteRepository = noteRepository;
+    this._eventBus = eventBus;
   }
 
   process(note: Note): string {
-    return this._noteRepository.save(note);
+    const eventTopic = 'NOTE';
+
+    const id = this._noteRepository.save(note);
+    this._eventBus.push(eventTopic, id);
+
+    return id;
   }
 }
