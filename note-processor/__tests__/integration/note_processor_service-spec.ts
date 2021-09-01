a73ffaafdf373f658c437144476cf82d3b0c9336
shladdergoo <shladdergoo@users.noreply.github.com>
2021-09-01 14:56:27 +0100

    stub integration test

---
+++ note-processor/__tests__/integration/note_processor_service-spec.ts
@@ -0,0 +1,8 @@
+import { EventBus } from '../../src/event_bus';
+import { Note } from '../../src/note';
+import { NoteRepository } from '../../src/note_repository';
+import { NoteProcessorService } from '../../src/note_processor_service'
+
+it('should process correctly', () => {
+
+});
